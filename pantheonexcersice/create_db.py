b7acf9bd488291c2411ef51780f62b1f10db0b14
neurobrko <elvis@elvis.sk>
2024-08-28 21:12:36 +0200

    02 - DB

--- pantheonexcersice/create_db.py
+++ pantheonexcersice/create_db.py
@@ -1,10 +1,28 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date
+from sqlalchemy import (
+    create_engine,
+    insert,
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    String,
+    Date,
+)
+from datetime import datetime
 
-engine = create_engine("sqlite://library.db", echo=True)
+
+def convert_date(dateString, dateFormat="%Y-%m-%d"):
+    dt = datetime.strptime(dateString, dateFormat)
+    return dt
+
+
+# create db
+engine = create_engine("sqlite:///library.db", echo=True)
 meta = MetaData()
 
-book = Table(
-    "book",
+# create table
+books = Table(
+    "books",
     meta,
     Column("id", Integer, primary_key=True),
     Column("title", String),
@@ -12,3 +30,16 @@ book = Table(
     Column("isbn", String),
     Column("published_date", Date),
 )
+meta.create_all(engine)
+
+# insert test data
+stmt = insert(books).values(
+    title="A Voyage for Madman",
+    author="Peter Nichols",
+    isbn="9780060957032",
+    published_date=convert_date("2002-06-04"),
+)
+
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    conn.commit()
